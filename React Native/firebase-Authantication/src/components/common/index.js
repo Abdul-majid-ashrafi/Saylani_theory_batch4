42c93bfd8652491bb2eae3ec746b974b885c8ae0
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-07 10:49:27 +0500

    create form

---
+++ React Native/firebase-Authantication/src/components/common/index.js
@@ -0,0 +1,7 @@
+export * from './Header'
+// export * from './List'
+export * from './Card'
+export * from './CardSection'
+export * from './Button'
+export * from './Input'
+// import {} from './home'
---
+++ React Native/firebase-Authantication/src/components/index.js
@@ -0,0 +1,3 @@
+// import {Form} from './form'
+
+export * from './form'
