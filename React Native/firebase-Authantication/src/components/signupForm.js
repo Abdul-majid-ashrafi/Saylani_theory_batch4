fdc18df5a611c584f5f3dc093cae5c5f541c7219
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-13 14:55:33 +0500

    Improvement to signup form and implement spinner for loading

--- React Native/firebase-Authantication/src/components/signupForm.js
+++ React Native/firebase-Authantication/src/components/signupForm.js
@@ -1,36 +1,56 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import firebase from 'firebase'
-import { Button, Card, CardSection, Input } from './common'
+import { Button, Card, CardSection, Input, Spinner } from './common'
 
 export class SignupForm extends Component {
-    state = { userFullName: '', email: '', password: '', error: '' }
 
-
+    state = {
+        userFullName: 'user 1',
+        email: 'user@gmail.com',
+        password: 'user',
+        error: '',
+        loading: false
+    }
 
     Signup() {
         const { email, password } = this.state
-        this.setState({ error: '' })
+        this.setState({ error: '', loading: true })
 
         firebase.auth().createUserWithEmailAndPassword(email, password)
             // Handle respnse here
             .then((response) => {
                 firebase.database().ref('/').child(`nativeUser/${response.uid}`).set(this.state)
+                this.nowSignupSuccess()
                 console.log("ResPonSe", response)
             })
             .catch((error) => {
                 // Handle Errors here.
                 var errorMessage = error.message;
-                this.setState({ error: errorMessage })
+                this.setState({ error: errorMessage, loading: false })
                 console.log(errorMessage);
             });
+    }
+
+    nowSignupSuccess() {
+        this.setState({ email: '', password: '', loading: false })
+    }
+
+    renderButtonAndLoader() {
+        if (this.state.loading) {
+            return <Spinner />
+        }
+        return (
+            <Button onPress={this.Signup.bind(this)}>
+                Submit
+            </Button>
+        )
     }
 
 
     render() {
         return (
             <View>
-
                 <Card>
                     <CardSection>
                         <Input
@@ -61,22 +81,15 @@ export class SignupForm extends Component {
                     </CardSection>
 
                     <CardSection>
-                        <Button onPress={this.Signup.bind(this)}>
-                            Submit
-                        </Button>
-                    </CardSection>
-
-                    <CardSection>
                         <Text style={errorStyle}>
                             {this.state.error}
                         </Text>
                     </CardSection>
+
+                    <CardSection>
+                        {this.renderButtonAndLoader()}
+                    </CardSection>
                 </Card>
-
-
-                {/*<Text>
-                    Render Form Page
-               </Text>*/}
             </View>
         )
     }
