06246484666967540edf4155bfe669cb24e03ca6
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-28 09:36:47 +0500

    Create About Component

---
+++ React Native/AwesomeNavigation/src/about.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+
+export class AboutComponent extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Hello About Component
+                </Text>
+            </View>
+        )
+    }
+}
